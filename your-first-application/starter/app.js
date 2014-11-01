d17f613e4c4f116a1cc37782ae057a6ab96033e2
Philippe Vinchon <p.vinchon@gmail.com>
2014-11-01 15:47:27 +0000

    finished first lab

---
+++ your-first-application/starter/app.js
@@ -0,0 +1,6 @@
+var tabs = document.querySelector('paper-tabs');
+var list = document.querySelector('post-list');
+
+tabs.addEventListener('core-select', function() {
+  list.show = tabs.selected;
+});
